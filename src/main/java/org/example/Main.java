b38d25291cca12bda2c1d99000527cf3e29e0aae
Zjx <32800464@qq.com>
2025-04-16 22:24:01 +0800

    test03

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
-import org.example.feature;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        feature f=new feature();
+        Feature f=new Feature();
         int result = f.add(5,3);
         System.out.println("Result: " + result);
         for (int i = 1; i <= 5; i++) {
