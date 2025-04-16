7c538198241ab3ea023798b458c901c20c33778b
Zjx <32800464@qq.com>
2025-04-16 22:22:12 +0800

    test02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,12 @@
 package org.example;
-
+import org.example.feature;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
+        feature f=new feature();
+        int result = f.add(5,3);
+        System.out.println("Result: " + result);
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
